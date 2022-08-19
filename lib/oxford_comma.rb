b62c0f1babe0fe7a4d7b636562ef9c6d4cfcb8e8
auma2022 <auma.amwayi@student.moringaschool.com>
2022-08-19 13:01:47 +0300

    commit tree 64e1a77885cf07f751f7998c783e1c17e84ec768

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+   
+    if array.length >= 3 
+        arr= array.delete_at(array.length-1)
+        return ("#{array.join(', ')}, and #{arr}" )
+    elsif array.length == 2
+        return array.join(" and ")
+    elsif array.length == 1
+        return array.join    
+    end         
 end
+puts oxford_comma(["fiddleheads", "okra", "kohlrabi"])
